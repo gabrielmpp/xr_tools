e6a5233f4386b9b1da6abb7a6ca6617134ef3694
gab <gabrielmpp2@gmail.com>
2020-07-19 20:49:39 +0100

    update

--- concat_simulation_output.py
+++ concat_simulation_output.py
@@ -8,8 +8,9 @@ simpath = sys.argv[1]
 sim = simpath.split('/')[-1]
 outpath = '/group_workspaces/jasmin4/upscale/gmpp/convzones/'
 files = [f for f in glob.glob(simpath + "**/*.nc", recursive=True)]
+print(files)
 da = xr.open_mfdataset(files).to_array().isel(variable=0).drop('variable')
-da = da.chunk({'time': 100})
+# da = da.chunk({'time': 100})
 try:
     os.makedirs(outpath + sim)
 except FileExistsError:
