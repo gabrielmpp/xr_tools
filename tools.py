07417552d7d394315d6b0ef55d55deab23259c73
gab <gabrielmpp2@gmail.com>
2022-05-16 16:21:50 +0100

    updates

--- tools.py
+++ tools.py
@@ -18,6 +18,20 @@ import xesmf as xe
 from warnings import warn
 
 
+def full_stack_error():
+    import traceback, sys
+    exc = sys.exc_info()[0]
+    stack = traceback.extract_stack()[:-1]  # last one would be full_stack()
+    if exc is not None:  # i.e. an exception is present
+        del stack[-1]       # remove call of full_stack, the printed exception
+                            # will contain the caught exception caller instead
+    trc = 'Traceback (most recent call last):\n'
+    stackstr = trc + ''.join(traceback.format_list(stack))
+    if exc is not None:
+         stackstr += '  ' + traceback.format_exc().lstrip(trc)
+    return stackstr
+
+
 def regrid_ds1_on_ds2(ds1, ds2, method='conservative',
                  latname='latitude', lonname='longitude', copy=True):
     if copy:
